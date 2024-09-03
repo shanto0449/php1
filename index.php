c80e399da9fab3719158e5a9ecf80c1bcb359d9a
shanto0449 <shantohossen22@gmail.com>
2024-09-02 23:32:51 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    function test_name($name, $age){
+        echo "This is function $name and his age is $age <br>";
+
+    }
+    test_name("shanto",25);
+    test_name("Ali",28);
+
+    ?>
+</body>
+</html> 
