f4ddf199f4cf29e48f04ba89302baf4994669e6d
shanto0449 <shantohossen22@gmail.com>
2024-09-02 23:50:17 -0700

    First release of Hello World!

--- index.php
+++ index.php
@@ -7,12 +7,18 @@
 </head>
 <body>
     <?php 
-    function test_name($name, $age){
-        echo "This is function $name and his age is $age <br>";
+    // function test_name($name, $age){
+    //     echo "This is function $name and his age is $age <br>";
 
-    }
-    test_name("shanto",25);
-    test_name("Ali",28);
+    // }
+    // test_name("shanto",25);
+    // test_name("Ali",28);
+
+    // Date and Time php
+    // echo "today is" .date("y/m/d") ."<br>";
+
+    date_default_timezone_set("Asia/Dhaka");
+    echo "today is" .date("h:i:sa") ."<br>";
 
     ?>
 </body>
